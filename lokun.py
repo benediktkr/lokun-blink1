3e0c5fe66c6fa17c939033998c69dfc2c7d8ae01
Benedikt Kristinsson <benedikt.k@gmail.com>
2014-06-01 19:51:12 +0000

    cleanup

--- lokun.py
+++ lokun.py
@@ -9,22 +9,25 @@ from blink1 import Blink1
 # IP?
 API = "https://api.lokun.is"
 
-def rgb(color):
-    if color.lower() == "red":
+def rgb(status):
+    if status.lower() == "red":
         return [255, 0, 0]
-    if color.lower() == "yellow":
+    if status.lower() == "yellow":
         return [232, 181, 0]
-    if color.lower() == "green":
-        return [0, 255, 0]
-    if color.lower() == "blue":
+    if status.lower() == "green":
+        #return [0, 255, 0]
+        return [0, 0, 0]
+    if status.lower() == "blue":
         return [0, 0, 255]
 
-    raise ValueError("red, yellow or green")
+    return rgb("blue")
 
 def get_status():
     try:
         json = requests.get(API + "/lokun/status", timeout=4.20).json()
         return json["status"]
+    except IndexError:
+        return "red"
     except Exception as ex:
         try:
             requests.get("http://google.com", timeout=4.20)
@@ -50,10 +53,9 @@ def set_color(blink1, status, flash=False):
 
 def main(color=""):
     status = color or get_status()
-    flash_light = status == "green"
     print "Status: " + status
     blink1 = Blink1()
-    set_color(blink1, status, flash_light)
+    set_color(blink1, status)
     return 0
 
 if __name__ == "__main__":
